111208fa9b849d7237ad82e30a2f9ccbbab99ffc
Abdel7004 <abdelrahmanh15@gmail.com>
2023-03-23 07:40:46 -0400

    Take One Down, Pass it Around App Created

--- server.js
+++ server.js
@@ -54,3 +54,22 @@ app.get("/magic/:question", (req, res) => {
 
   res.send(`<h1>${question}?</h1><p>${response}</p>`);
 });
+
+// TAKE ONE DOWN AND PASS IT AROUND
+let numBottles = 99;
+
+app.get("/", (req, res) => {
+  res.send(`<h1>${numBottles} Bottles of beer on the wall</h1>
+            <a href="/${numBottles - 1}">Take one down, pass it around</a>`);
+});
+
+app.get("/:num", (req, res) => {
+  let num = parseInt(req.params.num);
+  if (num === 0) {
+    res.send(`<h1>No more bottles of beer on the wall!</h1>
+              <a href="/">Start over</a>`);
+  } else {
+    res.send(`<h1>${num} Bottles of beer on the wall</h1>
+              <a href="/${num - 1}">Take one down, pass it around</a>`);
+  }
+});
